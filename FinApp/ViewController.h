75d0f6439331611a0c669dece888e63a98219d3b
Sidd Singh <sidd@Sidds-MacBook-Air.local>
2014-10-08 22:34:23 -0700

    Initial Commit

---
+++ FinApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FinApp
+//
+//  Created by Sidd Singh on 10/8/14.
+//  Copyright (c) 2014 Sidd Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FinApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FinApp
+//
+//  Created by Sidd Singh on 10/8/14.
+//  Copyright (c) 2014 Sidd Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
