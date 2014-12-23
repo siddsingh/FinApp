2a76cd777d8d584f9e224bd3ef4dff802844781e
Sidd Singh <sidd.singh@gmail.com>
2014-12-22 19:55:09 -0800

    Added custom view controller to manage the upcoming events view

---
+++ FinApp/FAUpcomingEventsViewController.h
@@ -0,0 +1,20 @@
+//
+//  FAUpcomingEventsViewController.h
+//  FinApp
+//
+//  Class that manages the view showing upcoming events.
+//
+//  Created by Sidd Singh on 12/18/14.
+//  Copyright (c) 2014 Sidd Singh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FAUpcomingEventsViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+
+
+// Table for list of events
+@property (weak, nonatomic) IBOutlet UITableView *eventsListTable;
+
+
+@end
